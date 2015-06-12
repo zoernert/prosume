a39780dda2ff36bec46f1eac5e6db3a449b3d50c
Thorsten Zoerner <me@thorsten-zoerner.com>
2015-06-12 02:44:24 +0200

    Added Packages for Roles

---
+++ gra/Prosume/src/de/prosume/roles/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Rollen sind bei Prosume die verschiedenen Arten von Clients.
+ *  - Broker = Messstellenbetreiber 
+ *  - Producer = Messstelle eines Erzeugers
+ *  - Consumer = Messstelle eines Verbrauchers
+ *  - Congestion = Ausgleichenergieanbieter (Positiv/Negativ)
+ */
+/**
+ * @author Thorsten Zoerner
+ *
+ */
+package de.prosume.roles;
