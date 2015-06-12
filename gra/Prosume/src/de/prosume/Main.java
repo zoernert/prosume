bb01d8d94df0682c08de544e3ee49a12f57845e3
Thorsten Zoerner <me@thorsten-zoerner.com>
2015-06-12 02:25:21 +0200

    Create Repository

---
+++ gra/Prosume/src/de/prosume/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package de.prosume;
+
+/**
+ * Startet das Testsystem
+ * 
+ * @author Thorsten Zoerner
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
